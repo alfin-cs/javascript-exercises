df8deee668353dbb53ecd3565f04ece04d533f47
alfin-cs <alfinsathesh@gmail.com>
2025-08-09 23:58:50 +0300

    going to delete everything

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let obj={}
+    let present=1
+    let last=0
+    let sequenceNum=0
+    for (let i=1;i<num;i++){
+        sequenceNum=present+last
+        last=present
+        present=sequenceNum
+    }
+    console.log(sequenceNum)
+    return sequenceNum
 };
-
+fibonacci(4)
 // Do not edit below this line
 module.exports = fibonacci;
